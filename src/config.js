ff5aeeedcf89886ace5aafde8875da6f61659890
Đỗ Văn Hưng <dvhungk16@gmail.com>
2024-03-07 00:33:07 +0700

    first commit

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.instagram.com/p/C4F0sAnPsHoOul77fPth5J3ocNW3SJYLMXhT9o0/?hl=vi';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Tặng em';
+const contentCard = 'Nhân ngày 8/3, a chúc em a lờ lờ ^^^';
+
+
+const giftImage = '/img/Screenshot 2023-11-29 142105.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
